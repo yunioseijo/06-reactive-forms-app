2611468a64365c7a74d214293e28bdd9e98831f6
yunioseijo <yunioseijo@gmail.com>
2025-04-07 18:47:43 +0200

    implement reactive form for basic page; add validation and error handling for product fields

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,28 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent { }
+export class BasicPageComponent {
+  private fb = inject(FormBuilder);
+  myForm: FormGroup = this.fb.group({
+    name: ['',[Validators.required,Validators.minLength(3)],[]],
+    price:[0,[Validators.required,Validators.min(10)]] ,
+    inStorage: [0, [Validators.required,Validators.min(0)]],
+  });
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFiledError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors ?? {};
+
+    return null;
+  }
+ }
