092f7a363d0acf695a9bd2ab636f82effbd49d3d
yunioseijo <yunioseijo@gmail.com>
2025-04-10 13:09:46 +0200

    enhance switches page; implement reactive form with validation for gender, notifications, and terms acceptance

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,28 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
 })
-export class SwitchesPageComponent { }
+export class SwitchesPageComponent {
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true],
+    termAndConditions: [false, Validators.requiredTrue],
+  });
+
+
+  onSubmit() {
+  if (this.myForm.invalid) {
+    this.myForm.markAllAsTouched();
+    return;
+  }
+}
+
+}
